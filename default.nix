c0720dc449f77e1130c316f4198caf80b226be55
gtrunsec <gtrunsec@hardenedlinux.org>
2021-03-06 12:16:35 +0800

    Fix wrap bin path

--- default.nix
+++ default.nix
@@ -3,12 +3,11 @@ let
     (import ./packages-overlay.nix)
   ];
 
-  pkgs = (import <nixpkgs>) { inherit overlays;};
+  pkgs = (import <nixpkgs>) { inherit overlays; };
 in
-
 pkgs.buildEnv {
-    name = "test-spicy";
-    paths = with pkgs; [
-      spicy
-    ];
+  name = "test-spicy";
+  paths = with pkgs; [
+    spicy
+  ];
 }
--- spicy/default.nix
+++ spicy/default.nix
@@ -1,46 +1,64 @@
-{stdenv, cmake, flex, bison, python38, zlib, llvmPackages_9, fetchFromGitHub, which, ninja, lib
-, makeWrapper, glibc
+{ stdenv
+, cmake
+, flex
+, bison
+, python38
+, zlib
+, llvmPackages_9
+, fetchFromGitHub
+, which
+, ninja
+, lib
+, makeWrapper
+, glibc
 }:
 
 stdenv.mkDerivation rec {
-  version = "master";
+  version = "2021-03-05";
   name = "spicy";
   src = fetchFromGitHub {
     owner = "zeek";
     repo = "spicy";
-    rev = "193742cd1d197910dbc2b905e1e3ada7d968d480";
+    rev = "f1e2dc0f3e068864ee4dbc587af8b0f4aa19ed9b";
     fetchSubmodules = true;
-    sha256 = "sha256-7OaKsiyqLAR/eeVWwT/sk2KZYnI2yqjB4DuQH7b2/to=";
+    sha256 = "sha256-jHXgv/usPTCflnH5AqLXgYUyXDa4tN2i+fjFDoxMJbo=";
   };
 
 
-  nativeBuildInputs = [ cmake flex bison  python38 zlib
-                        #ninja
-                      ];
-  buildInputs = [ which
-                  llvmPackages_9.clang-unwrapped
-                  llvmPackages_9.llvm
-                  makeWrapper
-                ];
+  nativeBuildInputs = [
+    cmake
+    flex
+    bison
+    python38
+    zlib
+    #ninja
+  ];
+  buildInputs = [
+    which
+    llvmPackages_9.clang-unwrapped
+    llvmPackages_9.llvm
+    makeWrapper
+  ];
 
   preConfigure = ''
-   patchShebangs ./scripts/autogen-type-erased
-   patchShebangs ./scripts/autogen-dispatchers
-'';
+    patchShebangs ./scripts/autogen-type-erased
+    patchShebangs ./scripts/autogen-dispatchers
+  '';
+
+  patches = [ ./version.patch ];
 
   cmakeFlags = [
     "-DCMAKE_CXX_COMPILER=${llvmPackages_9.clang}/bin/clang++"
     "-DCMAKE_C_COMPILER=${llvmPackages_9.clang}/bin/clang"
-    "-DHILTI_HAVE_JIT=true"
   ];
 
   postFixup = ''
-    wrapProgram $out/bin/spicyc \
-      --set CLANG_PATH      "${llvmPackages_9.clang}/bin/clang" \
-      --set CLANGPP_PATH    "${llvmPackages_9.clang}/bin/clang++" \
-      --set CPATH           "${lib.makeSearchPathOutput "dev" "include" [ flex bison python38 zlib glibc llvmPackages_9.libcxxabi llvmPackages_9.libcxx ]}/c++/v1" \
-      --set LIBRARY_PATH    "${lib.makeLibraryPath [ flex bison python38 zlib glibc llvmPackages_9.libclang llvmPackages_9.libcxxabi llvmPackages_9.libcxx ]}" \
-      --set NIX_CFLAGS_LINK "-lc++abi -lc++"
+    for e in $(cd $out/bin && ls); do
+      wrapProgram $out/bin/$e \
+        --set CLANG_PATH      "${llvmPackages_9.clang}/bin/clang" \
+        --set CLANGPP_PATH    "${llvmPackages_9.clang}/bin/clang++" \
+        --set LIBRARY_PATH    "${lib.makeLibraryPath [ flex bison python38 zlib glibc llvmPackages_9.libclang llvmPackages_9.libcxxabi llvmPackages_9.libcxx ]}"
+     done
   '';
 
   meta = with stdenv.lib; {
